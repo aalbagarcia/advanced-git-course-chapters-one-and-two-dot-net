de16fbd0697e6b79db3f21550cd8d0904ef411cd
Alfonso Alba García <alfonso@alfonsoalba.com>
2019-03-01 19:24:49 +0100

    [mostrar_tweets] Refactoring: extrae la clase Twitter
    Este el último commit de la rama "mostrar_tweets". El código de este
    commit puede verse en el siguiente enlace:
    
    https://gist.github.com/aalbagarcia/8c4906004a2ad81d8831a663398a9cbf
    
    Antes de terminar la tarea, hacemos un pequeño refactoring y extraemos
    la clase Twitter con dos métodos para obtener el token y obtener el
    texto de los tweets.
    
    Una vez tenemos el código compilado y vemos que funciona realizamos este
    último commit dentro de la rama [mostrar_tweets].
    
    Una vez terminado el código, integramos el
    código en master ejecutando:
    
    > git checkout master
    > git merge master
    
    Como resultado del merge, que es fast-forward, la rama master apuntará
    a este commit (al igual que la rama mostrar_tweets).

---
+++ TwitterFeed/Twitter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+
+namespace TwitterFeed
+{
+    class Twitter
+    {
+        private string OAuthConsumerKey;
+        private string OAuthConsumerSecret;
+
+        public Twitter(string key, string secret)
+        {
+            OAuthConsumerKey = key;
+            OAuthConsumerSecret = secret;
+        }
+
+
+        private async Task<string> GetAccessToken()
+        {
+            Console.WriteLine("Requesting Access Token...");
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://api.twitter.com/oauth2/token");
+            var customerInfo = Convert.ToBase64String(new UTF8Encoding()
+                                      .GetBytes(OAuthConsumerKey + ":" + OAuthConsumerSecret));
+            request.Headers.Add("Authorization", "Basic " + customerInfo);
+            request.Content = new StringContent("grant_type=client_credentials",
+                                                    Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            string json = await response.Content.ReadAsStringAsync();
+            var serializer = new JavaScriptSerializer();
+            dynamic item = serializer.Deserialize<object>(json);
+            Console.WriteLine(item["access_token"]);
+            return item["access_token"];
+        }
+
+        public async Task<IEnumerable<string>> GetTweets(string userName, int count, string accessToken = null)
+        {
+            if (accessToken == null)
+            {
+                accessToken = await GetAccessToken();
+            }
+            Console.WriteLine("Requesting Twitts...");
+        
+            var requestUserTimeline = new HttpRequestMessage(HttpMethod.Get,
+                string.Format("https://api.twitter.com/1.1/statuses/user_timeline.json?count={0}&screen_name={1}&trim_user=1&exclude_replies=1", count, userName));
+
+            requestUserTimeline.Headers.Add("Authorization", "Bearer " + accessToken);
+            var httpClient = new HttpClient();
+            HttpResponseMessage responseUserTimeLine = await httpClient.SendAsync(requestUserTimeline);
+            var serializer = new JavaScriptSerializer();
+            dynamic json = serializer.Deserialize<object>(await responseUserTimeLine.Content.ReadAsStringAsync());
+            var enumerableTweets = (json as IEnumerable<dynamic>);
+
+            if (enumerableTweets == null)
+            {
+                return null;
+            }
+            return enumerableTweets.Select(t => (string)(t["text"].ToString()));
+        }
+    }
+}
